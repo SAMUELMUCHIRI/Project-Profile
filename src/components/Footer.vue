2e49957079a0e0b594252a9d1e71ba80b6c7541b
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-02-24 19:58:50 +0300

    Footer: creation of site footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <p
+        class="w-full bg-[#CFA029] text-white text-sm font-medium py-3 px-4 text-center transition-colors"
+    >
+        ForeFrontManagement @2025 All Rights Reserved.
+    </p>
+</template>
