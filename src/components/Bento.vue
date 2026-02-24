f73cce147c22e050e044f03cb2db1b4b268a3462
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-02-24 19:58:50 +0300

    Bento: creation of main bento container component

---
+++ src/components/Bento.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const data = defineProps({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    secondname: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true,
+    },
+    imgSrc: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div
+        class="rounded-xl grid grid-cols-1 sm:grid-cols-2 items-center justify-center gap-4"
+        :style="{ backgroundColor: color }"
+    >
+        <img
+            :src="imgSrc"
+            alt="Bento Image"
+            class="size-40 rounded-full object-cover object-top p-2"
+        />
+
+        <div class="text-center text-md-start">
+            <div class="py-4">
+                <p class="text-lg text-black">{{ firstname }}</p>
+                <p class="text-lg text-black">{{ secondname }}</p>
+            </div>
+            <p class="text-2xl font-bold text-black text font-serif">
+                {{ value }}
+            </p>
+        </div>
+    </div>
+</template>
