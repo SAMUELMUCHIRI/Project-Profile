4817677988cf1c04b632ce50d154f50736c1378c
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-02-24 19:58:50 +0300

    SectionGreat: creation of featured highlights section component

---
+++ src/components/SectionGreat.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex min-h-max justify-center items-center py-2">
+        <div class="w-full h-full flex-1 flex flex-col container mx-auto p-2">
+            <div class="px-5 bg-zinc-700 rounded-xl py-4">
+                <h2 class="text-[#CFA029] text-lg font-bold mb-2">
+                    Now let's make you great!
+                </h2>
+                <p class="text-[#CFA029] text-sm mb-3">
+                    You have shown us where you are doing well and where we can
+                    support you to be great!
+                </p>
+                <p class="text-zinc-300 text-sm leading-relaxed">
+                    From your profile we recommend these memberships. Pick on
+                    that fits you best and start your financial adventure
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
