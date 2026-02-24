07d13e5605c061307f252257b1bdb784da464be8
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-02-24 19:58:50 +0300

    CTA: creation of call-to-action component

---
+++ src/components/CTA.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex min-h-max justify-center items-center py-2">
+        <div class="w-full h-full flex-1 flex flex-col container mx-auto p-2">
+            <p
+                class="w-full bg-[#CFA029] text-white font-bold text-sm font-medium py-3 px-4 rounded-lg transition-colors"
+            >
+                A closer look at the memberships details is available here
+            </p>
+        </div>
+    </div>
+</template>
